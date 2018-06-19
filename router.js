7a941ed123fac01ee784655bf0ffbd249c203267
Monica Dinculescu <notwaldorf@gmail.com>
2018-06-19 14:27:46 -0700

    rework docs, add note about forcing a navigation (#30)
    * rework docs, add note about forcing a navigation

--- router.js
+++ router.js
@@ -12,20 +12,27 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   Basic router that calls a callback whenever the location is updated.
 
   Sample use:
-  
+
+    import { installRouter } from '../node_modules/pwa-helpers/router.js';
+    //...
+    installRouter((location) => _locationChanged(location));
+
+  For example, if you're using this router in a Redux-connected component,
+  you could dispatch an action in the callback:
+
     import { installRouter } from '../node_modules/pwa-helpers/router.js';
     import { navigate } from '../actions/app.js';
+    //...
+    installRouter((location) => store.dispatch(navigate(location)))
 
-  If you don’t have any other work to do other than dispatching an action,
-  you can write something like:
 
-    installRouter((location) => store.dispatch(navigate(location)));
+  If you need to force a navigation to a new location programmatically, you can
+  do so by pushing a new state using the History API, and then manually
+  calling the callback with the new location:
 
-  If you need to do other work, you can also use this, where the
-  _locationChanged method would dispatch the store action.
+    window.history.pushState({}, '', '/new-route');
+    _locationChanged(window.location);
 
-    installRouter((location) => this._locationChanged(location));
-  
   Optionally, you can use the second argument to read the event that caused the
   navigation. For example, you may want to scroll to top only after a link click.
 
